4abfa592af0a7a97fe48e4f9cb5bdb6aece6f042
Nebiyu <nebiyueliasonlycourses@gmail.com>
2026-02-09 13:46:48 +0300

    refactor: replace inline message handling with controller methods in home.js

--- routes/home.js
+++ routes/home.js
@@ -1,34 +1,10 @@
 const { Router } = require("express");
 const router = Router();
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
+const controller = require("../controllers/messageController");
 
-router.post("/new", (req, res) => {
-  const { user, text } = req.body;
-  messages.push({ text, user, added: new Date() });
-  res.redirect("/");
-});
+router.get("/", controller.userList);
+router.post("/new", controller.addMessage);
+router.get("/new", controller.newMessageForm);
+router.get("/message/:id", controller.messageDetails);
 
-router.get("/new", (req, res) => {
-  res.render("form", { title: "New Message" });
-});
-router.get("/message/:id", (req, res) => {
-  const { id } = req.params;
-  const message = messages[id];
-  if (!message) {
-    return res.status(404).send("Message not found");
-  }
-  res.render("detalis", { message });
-});
-
-module.exports = { messages, router };
+module.exports = { router };
